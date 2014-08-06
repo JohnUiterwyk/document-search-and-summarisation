ba996a54c901d91b982bc66e6f247202ed12b135
John Uiterwyk <jnuiterwyk@gmail.com>
2014-08-06 20:51:15 +1000

    create project skeleton, add some directories and starter classes

---
+++ data/readme.md
@@ -0,0 +1,3 @@
+Data files are ignored by the git repo.
+But for consistency,we should both have data files here (latimes, stoplist).
+This file is just to make git track the directory.
---
+++ src/inforet/search.java
@@ -0,0 +1,15 @@
+package inforet;
+
+/**
+ * Created by johnuiterwyk on 6/08/2014.
+ *
+ * This file is the main entry point for the search component
+ */
+
+public class search {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("search app");
+    }
+}
