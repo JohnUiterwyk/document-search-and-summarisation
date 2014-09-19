7218f81ddbabe31f9ec3bb84af2e2ecb4ce60fb4
John Uiterwyk <jnuiterwyk@gmail.com>
2014-09-19 23:57:31 +1000

    partial working new search args class

--- src/inforet/util/QueryArgs.java
+++ src/inforet/util/QueryArgs.java
@@ -1,19 +1,25 @@
 package inforet.util;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Daniel on 16/08/2014.
  */
 public class QueryArgs {
 
+    public Boolean bm25Enabled = false;
+    public String queryLabel = "";
+    public Integer maxResults = 10;
+
     public String lexiconPath = "";
     public String invlistPath = "";
     public String mapPath     = "";
+    public String stopListPath = "";
+
     public String queryString = null;
+
 
     //Configurations
     private static int minArgs = 3;
@@ -34,39 +40,81 @@ public class QueryArgs {
      * @param args commandline arguments
      * @throws FileNotFoundException
      */
-    public void parseArgs(String[] args) throws FileNotFoundException {
+    public void parseArgs(String[] args) throws FileNotFoundException
+    {
+        for ( int i = 0; i < args.length; i++)
+        {
+            String arg = args[i];
+            if(arg.equals("-BM25"))
+            {
+                bm25Enabled = true;
 
-        //Error Checking - Checking existance only for now
-        if ( args.length < minArgs ){
-            usage();
-        }
-        else if ( ! new File(args[0]).exists() ){ //Lexicon file exists?
-            usage();
-            throw new FileNotFoundException("arg[0] file not found :" + args[0]);
-        }
-        else if ( ! new File(args[1]).exists() ){ //InvList file exists?
-            usage();
-            throw new FileNotFoundException("arg[1] file not found :" + args[1]);
-        }
-        else if ( ! new File(args[2]).exists() ){ //Map file exists?
-            usage();
-            throw new FileNotFoundException("arg[2] file not found :" + args[2]);
+            }else if(arg.equals("-q"))
+            {
+               i++;
+               queryLabel = args[i];
+            }else if(arg.equals("-n"))
+            {
+                i++;
+                maxResults = Integer.parseInt(args[i]);
+            }else if(arg.equals("-l"))
+            {
+                i++;
+                lexiconPath = args[i];
+                checkFile(lexiconPath);
+            }else if(arg.equals("-i"))
+            {
+                i++;
+                invlistPath = args[i];
+                checkFile(invlistPath);
+            }else if(arg.equals("-m"))
+            {
+                i++;
+                mapPath = args[i];
+                checkFile(mapPath);
+            }else if(arg.equals("-s"))
+            {
+                i++;
+                stopListPath = args[i];
+                checkFile(stopListPath);
+            }else
+            {
+                StringBuilder sb = new StringBuilder();
+                for ( ; i < args.length; i++){
+                    sb.append(args[i]);
+                    sb.append(" ");
+                }
+                queryString = sb.toString();
+            }
+
         }
 
 
         //Set the Files
-        lexiconPath = args[0];
-        invlistPath = args[1];
-        mapPath     = args[2];
 
+
+    }
+
+    public Boolean checkFile(String filePath) throws FileNotFoundException
+    {
+        if ( ! new File(filePath).exists() ){ //Map file exists?
+            usage();
+            throw new FileNotFoundException(filePath + "file not found.");
+        }else
+        {
+            return true;
+        }
+    }
+
+    public String parseQueryString(int firstTerm,String args[])
+    {
         //Set the query terms
-        queryString = new String();
         StringBuilder sb = new StringBuilder();
-        for ( int i = 3; i < args.length; i++){
+        for ( int i = firstTerm; i < args.length; i++){
             sb.append(args[i]);
             sb.append(" ");
         }
-        queryString = sb.toString();
+        return sb.toString();
     }
 
     /***
