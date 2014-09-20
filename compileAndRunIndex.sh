f770fa34c2f8defced8d372af14cab5ce0b96675
John Uiterwyk <jnuiterwyk@gmail.com>
2014-09-21 02:42:12 +1000

    tweaked bash scripts to use latimes_med

--- compileAndRunIndex.sh
+++ compileAndRunIndex.sh
@@ -1,3 +1,3 @@
 mkdir -p bin
 javac -d bin -sourcepath src src/inforet/index.java
-java -cp bin inforet.index -s data/stoplist data/latimes
+java -cp bin inforet.index -s data/stoplist data/latimes_med.txt
