88ca798c0430f6ad8ebcf0b35690c4a75108bdfe
John Uiterwyk <jnuiterwyk@gmail.com>
2014-09-22 00:16:54 +1000

    modified run script to use real collection when on rmit servers

--- compileAndRunIndex.sh
+++ compileAndRunIndex.sh
@@ -1,3 +1,11 @@
+STOP_LIST=data/stoplist
+DOC_COLLECTION=data/latimes_med.txt
+
 mkdir -p bin
 javac -d bin -sourcepath src src/inforet/index.java
-java -cp bin inforet.index -s data/stoplist data/latimes_med.txt
+if [ -d /home/sl1/S3406031 ]
+  then
+    DOC_COLLECTION=/KDrive/SEH/SCSIT/Students/Courses/ISYS1078/2014/a2/latimes
+fi
+
+java -cp bin inforet.index -s $STOP_LIST $DOC_COLLECTION
