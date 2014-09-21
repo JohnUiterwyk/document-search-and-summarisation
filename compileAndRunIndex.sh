b62e102659adff00d2c185c4c9ce07ec8613fac6
John Uiterwyk <jnuiterwyk@gmail.com>
2014-09-22 00:17:18 +1000

    tweak run script

--- compileAndRunIndex.sh
+++ compileAndRunIndex.sh
@@ -3,7 +3,7 @@ DOC_COLLECTION=data/latimes_med.txt
 
 mkdir -p bin
 javac -d bin -sourcepath src src/inforet/index.java
-if [ -d /home/sl1/S3406031 ]
+if [ -d /home/sl1 ]
   then
     DOC_COLLECTION=/KDrive/SEH/SCSIT/Students/Courses/ISYS1078/2014/a2/latimes
 fi
