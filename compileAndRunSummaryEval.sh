13305733361b39046cddad12e5c27eeca3ee7d0f
John Uiterwyk <jnuiterwyk@gmail.com>
2014-10-09 22:32:22 +1100

    changed summary eval count to 10

--- compileAndRunSummaryEval.sh
+++ compileAndRunSummaryEval.sh
@@ -1,3 +1,3 @@
 mkdir -p bin
 javac -d bin -sourcepath src src/inforet/summaryeval.java
-java -cp bin inforet.summaryeval -BM25  -n 20 -l lexicon -i invlists -m map -s data/stoplist -d data/latimes
+java -cp bin inforet.summaryeval -BM25  -n 10 -l lexicon -i invlists -m map -s data/stoplist -d data/latimes
